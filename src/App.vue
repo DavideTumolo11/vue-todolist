75cc6953ae98a2d5c5ca2c66538893d1efb1ad9d
DavideTumolo11 <davidetumolo11@gmail.com>
2024-01-29 18:43:02 +0100

    true-false

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,35 @@
 <script>
-
+export default {
+  name: 'App',
+  data() {
+    return {
+      task: [
+        {
+          text: 'learn HTML',
+          done: true
+        },
+        {
+          text: 'learn CSS',
+          done: true
+        },
+        {
+          text: 'learn Vue',
+          done: false
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
+  <h1>Todo List</h1>
+  <ul>
+    <li v-for="task in task">
+      <span :style="{ textDecoration: task.done ? 'line-through' : '' }">{{ task.text }}</span>
 
-  </div>
+    </li>
+  </ul>
 </template>
 
 <style></style>
