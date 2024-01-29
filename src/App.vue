51d17f4b7abd374e109e8fa295789bc0ced8ddea
DavideTumolo11 <davidetumolo11@gmail.com>
2024-01-29 19:04:11 +0100

    v-if

--- src/App.vue
+++ src/App.vue
@@ -18,18 +18,34 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    removetask(index) {
+      console.log('rimosso', index);
+      this.task.splice(index, 1)
+    }
   }
 }
 </script>
 
 <template>
   <h1>Todo List</h1>
-  <ul>
-    <li v-for="task in task">
+  <ul v-if="task.length > 0">
+    <li v-for="(task, index) in task">
       <span :style="{ textDecoration: task.done ? 'line-through' : '' }">{{ task.text }}</span>
+
+      <span v-on:click="removetask(index)">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-circle"
+          viewBox="0 0 16 16">
+          <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+          <path
+            d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+        </svg>
+      </span>
 
     </li>
   </ul>
+  <p v-else> non hai todo</p>
 </template>
 
 <style></style>
